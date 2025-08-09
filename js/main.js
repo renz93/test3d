27c7cfe62106a39e11e2fed7c3c998519a7c1629
renz93 <renzgha@gmail.com>
2025-08-09 07:46:12 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'eye';
+let objToRender = 'dino';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
